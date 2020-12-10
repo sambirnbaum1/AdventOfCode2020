b46e2771dcec3436a77c9b332f5c6d422fa55e32
sam birnbaum <birnbaum.sam@gmail.com>
2020-12-10 11:48:28 +0100

    I think d10p01 should have been wrong

--- adventOfCode/d10p01AdapterArray.py
+++ adventOfCode/d10p01AdapterArray.py
@@ -5,5 +5,5 @@ FILENAME = 'inputs/adapter_array.txt'
 
 @solver(FILENAME, int, sorted)
 def solve(jolts):
-    d1 = sum(j2 - j1 == 1 for j1, j2 in zip([0] + jolts, jolts))
-    return d1 * (len(jolts) - d1 + 1)
+    d1, d3 = map(sum, zip(*((j2 - j1 == 1, j2 - j1 == 3) for j1, j2 in zip([0] + jolts, jolts))))
+    return d1 * (d3 + 1)
