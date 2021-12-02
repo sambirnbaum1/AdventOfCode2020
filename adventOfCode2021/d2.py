79b92fef8f2ef567ac7e8dc4a89bbfebe40de712
sambi <birnbaum.sam@gmail.com>
2021-12-03 00:47:44 +0100

    'improved' D2 part2

--- adventOfCode2021/d2.py
+++ adventOfCode2021/d2.py
@@ -10,18 +10,22 @@ def parse_input(input_: Iterable[str]) -> Iterable[Tuple[str, int]]:
     return ((lambda dir_, dis: (dir_, int(dis)))(*inst.split(' ')) for inst in input_)
 
 
+def prod_part(i: int) -> int:
+    return int(i.real * i.imag)
+
+
 def part1(input_: Iterable[str]) -> int:
-    return (lambda i: int(i.real * i.imag))(sum(dis * DIRECTIONS[dir_] for dir_, dis in parse_input(input_)))
+    return prod_part(sum(dis * DIRECTIONS[dir_] for dir_, dis in parse_input(input_)))
 
 
 def part2(input_: Iterable[str]) -> int:
-    parsed_input = tuple(parse_input(input_))
-    aims = accumulate(parsed_input, (lambda aim, inst: aim + AIM_DIRECTIONS.get(inst[0], 0) * inst[1]), initial=0)
-    x = reduce(
+    return prod_part(reduce(
         lambda a, b: a + (b[0] == 'forward') * b[1] * (1 + b[2]*1j),
-        ((*inst, aim) for inst, aim in zip(parsed_input, aims)), 0
-    )
-    return int(x.real * x.imag)
+        accumulate(
+            parse_input(input_),
+            (lambda last, inst: (*inst, last[2] + AIM_DIRECTIONS.get(inst[0], 0) * inst[1])), initial=(0, 0, 0)
+        ), 0
+    ))
 
 
 if __name__ == '__main__':
