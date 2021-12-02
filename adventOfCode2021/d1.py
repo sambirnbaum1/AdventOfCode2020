caed710f83d63f32c62c774a65f118c1a2a1bb83
sambi <birnbaum.sam@gmail.com>
2021-12-02 23:49:21 +0100

    Started 2021

---
+++ adventOfCode2021/d1.py
@@ -0,0 +1,18 @@
+from timeit import timeit
+from typing import Tuple
+
+
+def part1(input_: Tuple[int]) -> int:
+    return sum(1 for height1, height2 in zip(input_, input_[1:]) if height2 > height1)
+
+
+def part2(input_: Tuple[int]) -> int:
+    return part1(tuple(sum(window) for window in zip(*(input_[start:] for start in range(3)))))
+
+
+if __name__ == '__main__':
+    with open('inputs/d1.txt', 'r') as fi:
+        input_ = tuple(int(height) for height in fi)
+
+    print(part1(input_))
+    print(part2(input_))
---
+++ adventOfCode2021/d2.py
@@ -0,0 +1,32 @@
+from typing import Iterable, Tuple
+from itertools import accumulate
+from functools import reduce
+
+DIRECTIONS = {'forward': 1, 'down': 1j, 'up': -1j}
+AIM_DIRECTIONS = {'down': 1, 'up': -1}
+
+
+def parse_input(input_: Iterable[str]) -> Iterable[Tuple[str, int]]:
+    return ((lambda dir_, dis: (dir_, int(dis)))(*inst.split(' ')) for inst in input_)
+
+
+def part1(input_: Iterable[str]) -> int:
+    return (lambda i: int(i.real * i.imag))(sum(dis * DIRECTIONS[dir_] for dir_, dis in parse_input(input_)))
+
+
+def part2(input_: Iterable[str]) -> int:
+    parsed_input = tuple(parse_input(input_))
+    aims = accumulate(parsed_input, (lambda aim, inst: aim + AIM_DIRECTIONS.get(inst[0], 0) * inst[1]), initial=0)
+    x = reduce(
+        lambda a, b: a + (b[0] == 'forward') * b[1] * (1 + b[2]*1j),
+        ((*inst, aim) for inst, aim in zip(parsed_input, aims)), 0
+    )
+    return int(x.real * x.imag)
+
+if __name__ == '__main__':
+    with open('inputs/d2.txt', 'r') as fi:
+        input_ = tuple(fi)
+
+    print(part1(input_))
+    print(part2(input_))
+
