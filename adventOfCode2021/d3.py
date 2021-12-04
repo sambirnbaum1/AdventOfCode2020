fd0ecc82677578207c0125e19438e5472e929d04
sambi <birnbaum.sam@gmail.com>
2021-12-04 11:12:29 +0100

    Fixed d3 typehints

--- adventOfCode2021/d3.py
+++ adventOfCode2021/d3.py
@@ -3,15 +3,15 @@ from typing import Tuple, Callable
 from functools import reduce
 
 
-def part1(input_: Tuple[str]) -> int:
+def part1(input_: Tuple[str, ...]) -> int:
     return (lambda gamma: gamma * (gamma ^ ((1 << (gamma.bit_length())) - 1)))(reduce(
         lambda val, cnt: (val << 1) | (cnt >= len(input_) / 2),
         (sum(bit == '1' for bit in col) for col in zip(*input_)), 0
     ))
 
 
-def part2(input_: Tuple[str]) -> int:
-    def select(candidates: Tuple[str], i: int, choose1: Callable[[int, float], bool]):
+def part2(input_: Tuple[str, ...]) -> int:
+    def select(candidates: Tuple[str, ...], i: int, choose1: Callable[[int, float], bool]):
         keep = '01'[choose1(sum(c[i] == '1' for c in candidates), len(candidates) / 2)]
         return (len(candidates) == 1 and candidates) or tuple(c for c in candidates if c[i] == keep)
     return mul(*(
